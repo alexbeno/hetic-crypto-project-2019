ff0b9212b32400dc860e7887ea200a7ca5b54bf0
Thibault <melkir13@gmail.com>
2019-04-29 14:55:12 +0200

    [core] setup apollo server

---
+++ packages/hetic-graphql/apollo.config.js
@@ -0,0 +1,8 @@
+// Configuration file for Apollo VSCode and CLI
+// https://www.apollographql.com/docs/references/apollo-config
+
+module.exports = {
+  service: {
+    localSchemaFile: './src/schema.graphql',
+  },
+}
--- packages/hetic-graphql/src/server.ts
+++ packages/hetic-graphql/src/server.ts
@@ -1,2 +1,23 @@
-// Edit this file to setup the GraphQL Server
-console.log('Hello World')
+import { ApolloServer, gql } from 'apollo-server'
+import fs from 'fs'
+
+// The GraphQL schema
+const typeDefs = gql`
+  ${fs.readFileSync(__dirname.concat('/schema.graphql'), 'utf8')}
+`
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`)
+})
